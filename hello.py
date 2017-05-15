c6ec19c6149c040560500ec282a9e015b4d6e362
DavidBuchanan314 <d@vidbuchanan.co.uk>
2017-05-15 15:30:20 +0100

    more balls, more colours

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+import random
 
 WIDTH = 640
 HEIGHT = 480
@@ -8,13 +9,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption("PongAI")
 clock = pygame.time.Clock()
 
-ball = {
-	"x": WIDTH/2,
-	"y": HEIGHT/2,
-	"xvel": 10,
-	"yvel": 10,
-	"r": 10
-}
+balls = []
+
+for _ in range(500):
+	ball = {
+		"x": random.randrange(0, WIDTH),
+		"y": random.randrange(0, HEIGHT),
+		"xvel": random.randrange(-10, 10),
+		"yvel": random.randrange(-10, 10),
+		"r": 10,
+		"color": (random.randrange(0, 0xFF), random.randrange(0, 0xFF), random.randrange(0, 0xFF))
+	}
+	balls.append(ball)
 
 while True:
 	for event in pygame.event.get():
@@ -22,12 +28,15 @@ while True:
 			pygame.quit()
 			sys.exit()
 	screen.fill((0, 0, 0)) # clear the screen with black
-	pygame.draw.rect(screen, (0xFF, 0xFF, 0xFF), (ball["x"]-ball["r"], ball["y"]-ball["r"], ball["r"]*2, ball["r"]*2), 0)
-	ball["x"] += ball["xvel"]
-	ball["y"] += ball["yvel"]
-	if (ball["xvel"] > 0 and ball["x"] + ball["r"] > WIDTH) or (ball["xvel"] < 0 and ball["x"] - ball["r"] < 0):
-		ball["xvel"] *= -1
-	if (ball["yvel"] > 0 and ball["y"] + ball["r"] > HEIGHT) or (ball["yvel"] < 0 and ball["y"] - ball["r"] < 0):
-		ball["yvel"] *= -1
+	
+	for ball in balls:
+		pygame.draw.rect(screen, ball["color"], (ball["x"]-ball["r"], ball["y"]-ball["r"], ball["r"]*2, ball["r"]*2), 0)
+		ball["x"] += ball["xvel"]
+		ball["y"] += ball["yvel"]
+		if (ball["xvel"] > 0 and ball["x"] + ball["r"] > WIDTH) or (ball["xvel"] < 0 and ball["x"] - ball["r"] < 0):
+			ball["xvel"] *= -1
+		if (ball["yvel"] > 0 and ball["y"] + ball["r"] > HEIGHT) or (ball["yvel"] < 0 and ball["y"] - ball["r"] < 0):
+			ball["yvel"] *= -1
+	
 	pygame.display.update()
 	clock.tick(60)
