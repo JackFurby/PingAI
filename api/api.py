91ee839e71864da5b88200e143647e61426f65e0
DavidBuchanan314 <d@vidbuchanan.co.uk>
2017-05-18 15:39:23 +0100

    Added simulation API and example usage

---
+++ api/api.py
@@ -0,0 +1,100 @@
+from enum import Enum
+import random
+
+
+class Action(Enum):
+	UP = "U"
+	DOWN = "D"
+	NONE = " "
+
+
+"""
+This class is a template. Extensions of this class should implement the "step" method
+"""
+class Agent:
+	def __init__(self):
+		self.score = 0
+		self.bat_y = 0.5 # this value is a fraction of the screen height
+		self.bat_height = 0.08
+		self.action = Action.NONE
+	
+	"""
+	This method should modify the `action` field to represent the desired action.
+	"""
+	def step(self, gamestate):
+		raise NotImplementedError
+
+
+class Game:
+	def __init__(self, agent_a, agent_b, options=None):
+		self.agent_a = agent_a
+		self.agent_b = agent_b
+		
+		self.options = {
+			"final_score": 10,
+			"bat_speed": 0.006,
+			"ball_speed": 0.015
+		}
+		
+		self.gamestate = {
+			"ball": {
+				"x": 0.5,
+				"y": 0.5,
+				"xvel": self.options["ball_speed"] * (random.randrange(0, 2)-0.5),
+				"yvel": self.options["ball_speed"] * (random.randrange(0, 2)-0.5) * 1.1,
+			}
+		}
+	
+	def step(self):
+		self.agent_a.step(self.gamestate)
+		flipped = self.gamestate.copy() # The other agent will have a mirrored perception of the game state
+		flipped["ball"] = flipped["ball"].copy()
+		flipped["ball"]["x"] = 1 - flipped["ball"]["x"]
+		flipped["ball"]["xvel"] *= -1
+		self.agent_b.step(flipped)
+		
+		for agent in [self.agent_a, self.agent_b]:
+			if agent.action == Action.UP:
+				agent.bat_y -= self.options["bat_speed"]
+				agent.bat_y = max(agent.bat_height, agent.bat_y)
+			elif agent.action == Action.DOWN:
+				agent.bat_y += self.options["bat_speed"]
+				agent.bat_y = min(1 - agent.bat_height, agent.bat_y)
+		
+		ball = self.gamestate["ball"]
+		x2 = ball["x"] + ball["xvel"]
+		y2 = ball["y"] + ball["yvel"]
+		
+		if x2 < 0:
+			if self.agent_a.bat_y - self.agent_a.bat_height < y2 < self.agent_a.bat_y + self.agent_a.bat_height: # bounce
+				x2 = -x2
+				ball["xvel"] *= -1
+			else: # don't bounce
+				self.agent_b.score += 1
+				x2 = 0.9
+				y2 = self.agent_b.bat_y
+		elif x2 > 1:
+			if self.agent_b.bat_y - self.agent_b.bat_height < y2 < self.agent_b.bat_y + self.agent_b.bat_height: # bounce
+				x2 = 2-x2
+				ball["xvel"] *= -1
+			else: # don't bounce
+				self.agent_a.score += 1
+				x2 = 0.1
+				y2 = self.agent_a.bat_y
+		if y2 < 0 or y2 > 1:
+			ball["yvel"] *= -1
+		
+		ball["x"] = x2
+		ball["y"] = y2
+
+	def play(self, renderer=None):
+		while max(self.agent_a.score, self.agent_b.score) < self.options["final_score"]:
+			self.step()
+			
+			if renderer is not None:
+				renderer(self.gamestate, self.agent_a, self.agent_b)
+		
+		if self.agent_a.score > self.agent_b.score:
+			return self.agent_a
+		else:
+			return self.agent_b
---
+++ api/apitest.py
@@ -0,0 +1,67 @@
+import pygame, sys
+from pygame.locals import *
+
+import api
+
+
+class KeyboardAgent(api.Agent):
+	def __init__(self, up_key, down_key):
+		api.Agent.__init__(self)
+		self.up_key = up_key
+		self.down_key = down_key
+	
+	def step(self, gamestate):
+		keys = pygame.key.get_pressed()
+		
+		if keys[self.up_key] and not keys[self.down_key]:
+			self.action = api.Action.UP
+		elif not keys[self.up_key] and keys[self.down_key]:
+			self.action = api.Action.DOWN
+		else: 
+			self.action = api.Action.NONE
+
+
+class NaiveAIAgent(api.Agent):
+	def step(self, gamestate):
+		if gamestate["ball"]["y"] < self.bat_y:
+			self.action = api.Action.UP
+		else:
+			self.action = api.Action.DOWN
+
+
+def pygame_render(gamestate, agent_a, agent_b):
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	
+	screen.fill(BLACK)
+	
+	pygame.draw.rect(screen, WHITE, ( 0, (agent_a.bat_y - agent_a.bat_height) * HEIGHT, 10, agent_a.bat_height * 2 * HEIGHT ), 0)
+	pygame.draw.rect(screen, WHITE, ( WIDTH-10, (agent_b.bat_y - agent_b.bat_height) * HEIGHT, 10, agent_b.bat_height * 2 * HEIGHT ), 0)
+	
+	pygame.draw.rect(screen, WHITE, ( (gamestate["ball"]["x"] * WIDTH) - 10, (gamestate["ball"]["y"] * HEIGHT) - 10, 20, 20), 0)
+	
+	pygame.display.update()
+	clock.tick(60)
+
+
+if __name__ == "__main__":
+	
+	WIDTH = 640
+	HEIGHT = 480
+	WHITE = (0xFF, 0xFF, 0xFF)
+	BLACK = (0x00, 0x00, 0x00)
+	
+	screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+	pygame.display.set_caption("PongAI")
+	
+	pygame.init()
+	clock = pygame.time.Clock()
+	
+	agent_a = KeyboardAgent(K_w, K_s)
+	#agent_b = KeyboardAgent(K_UP, K_DOWN)
+	agent_b = NaiveAIAgent()
+	
+	game = api.Game(agent_a, agent_b)
+	game.play(renderer=pygame_render)
